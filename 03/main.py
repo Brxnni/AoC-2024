bd21b2771bce5f4383d3ab0f4488988add9474ee
Brxnni <arne.norvin@gmail.com>
2024-12-03 08:55:27 +0100

    Day 03 (Shitty regex solution)

---
+++ 03/main.py
@@ -0,0 +1,32 @@
+import re
+
+import pathlib
+LOCAL = pathlib.Path(__file__).parent
+FILE = LOCAL / "input.txt"
+
+with open(FILE, "r") as file:
+	program = file.read()
+
+def part1(p):
+	# Shitty regex solution
+	pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+	instructions = re.findall(pattern, p)
+	mul = lambda x,y:int(x)*int(y)
+	return sum([ mul(*inst[4:-1].split(",")) for inst in instructions ])
+
+def part2(p):
+	# Extremely shitty regex solution
+	pattern = re.compile(r"(?:mul\(\d{1,3},\d{1,3}\))|(?:do(?:n't)?\(\))")
+	instructions = re.findall(pattern, p)
+	final = 0
+	do_status = True
+	for inst in instructions:
+		inst_type = inst.split("(")[0]
+		if inst_type == "don't": do_status = False
+		elif inst_type == "do": do_status = True
+		elif inst_type == "mul" and do_status:
+			x, y = inst[4:-1].split(",")
+			final += int(x)*int(y)
+	return final
+
+print(part2(program))
