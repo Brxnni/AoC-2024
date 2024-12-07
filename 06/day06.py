f4bf4b8d9732a4cebe65a09eb4f5d646732e0db9
Brxnni <arne.norvin@gmail.com>
2024-12-07 08:42:46 +0100

    Did Day 6 / Part 1 and Day / Parts 1 & 2

---
+++ 06/day06.py
@@ -0,0 +1,36 @@
+import pathlib
+LOCAL = pathlib.Path(__file__).parent
+
+def get_input(input_type = "input"):
+	with open(LOCAL / f"{input_type}.txt", "r", encoding="utf-8") as file:
+		return [ line.strip() for line in file.readlines() ]
+
+def part1(m):
+	height, width = len(m), len(m[0])
+
+	guard_position = [ (row_number, row.index("^")) for row_number, row in enumerate(m) if "^" in row ][0]
+	directions = [(-1,0), (0,1), (1,0), (0,-1)]
+	guard_direction = 0
+
+	obstacles = [ (row_i, col_i) for col_i in range(width) for row_i in range(height) if m[row_i][col_i] == "#"  ]
+
+	positions = set()
+	while True:
+		positions.add(guard_position)
+
+		new_position = tuple([ sum(x) for x in zip(guard_position, directions[guard_direction]) ])
+		if new_position not in obstacles:
+			guard_position = new_position
+
+			if not (0 <= guard_position[0] < height and 0 <= guard_position[1] < width):
+				break
+		else:
+			guard_direction = (guard_direction + 1) % 4
+
+		print(guard_position)
+
+	return len(positions)
+
+if __name__ == "__main__":
+    map_ = get_input("input")
+    print("P1 >>", part1(map_))
---
+++ 07/day07.py
@@ -0,0 +1,63 @@
+import pathlib
+LOCAL = pathlib.Path(__file__).parent
+
+def get_input(input_type = "input"):
+	with open(LOCAL / f"{input_type}.txt", "r", encoding="utf-8") as file:
+		return [(
+			int(line.split(": ")[0]),
+			list(map(int, line.strip().split(": ")[1].split(" ")))
+		) for line in file.readlines()]
+
+def part1(n):
+	count = 0
+	def check(res, nums):
+		if len(nums) == 1:
+			return nums[0] == res
+		return check(res - nums[-1], nums[:-1]) or check(res / nums[-1], nums[:-1])
+
+	for line in n:
+		if check(*line): count += line[0]
+	return count
+
+# Checks if the division would result in a float before calling the function recursively
+def part1_earlycheck(n):
+	count = 0
+	def check(res, nums):
+		if len(nums) == 1:
+			return nums[0] == res
+
+		if check(res - nums[-1], nums[:-1]): return True
+		if res % nums[-1] == 0:
+			if check(int(res / nums[-1]), nums[:-1]): return True
+
+		return False
+
+	for line in n:
+		if check(*line): count += line[0]
+	return count
+
+def part2(n):
+	count = 0
+	def check(res, nums):
+		if len(nums) == 1:
+			return nums[0] == res
+
+		if check(res - nums[-1], nums[:-1]):
+			return True
+		if res % nums[-1] == 0:
+			if check(int(res / nums[-1]), nums[:-1]): return True
+		if str(res).endswith(str(nums[-1])):
+			try:
+				if check(int(str(res)[:-len(str(nums[-1]))]), nums[:-1]): return True
+			except: ...
+
+		return False
+
+	for line in n:
+		if check(*line): count += line[0]
+	return count
+
+if __name__ == "__main__":
+	n = get_input("input")
+	print("P1 >>", part1(n))
+	print("P2 >>", part2(n))
